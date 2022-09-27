6c99ceacaf75b8652e66b98faa8ffdd0072c4eac
BEAR <ungnam0509@gmail.com>
2022-09-27 17:55:04 +0900

    Add files via upload

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -12,7 +12,7 @@ import MyPage from "./pages/MyPage";
 const App = () => {
   return (
     <Routes>
-      <Route element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profiles/:username" element={<Profile />} />
